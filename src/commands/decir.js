8684f8350c62c0900a1ea2420a6481bfb03c8697
TheGaspyGames <fritzariasgasparedu@outlook.es>
2025-11-29 15:03:42 -0300

    Agregar comando /decir para repetir texto

---
+++ src/commands/decir.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('decir')
+        .setDescription('Haz que el bot repita exactamente el texto que proporciones.')
+        .addStringOption(option =>
+            option
+                .setName('texto')
+                .setDescription('El mensaje que dirá el bot')
+                .setRequired(true)
+        ),
+
+    async execute(interaction) {
+        const message = interaction.options.getString('texto', true);
+        await interaction.reply({ content: message });
+    },
+};
